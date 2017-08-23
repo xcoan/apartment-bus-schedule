fa7d01e8c2f14042725395f56c8cdf9c23cf308a
X Coan <xjcoan@gmail.com>
2017-08-22 21:02:21 -0400

    Initial Commit

---
+++ www/js/baywater.js
@@ -0,0 +1,15 @@
+var data;
+var jqxhr = $.getJSON( "js/schedule.json", function(json) {
+  data = json;
+})
+  .done(function() {
+    var location = data["weekday"]["baywater"]["times"];
+    location.forEach(function(time) {
+      var markup="<tr><td>" + time + "</tr></td>";
+      $("table tbody").append(markup);
+      console.log(time);
+    });
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
